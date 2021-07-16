ff595add923a55f8592f22fe61219551b02ce952
jefffpierre <jeffpierre0@gmail.com>
2021-07-16 15:35:08 -0400

    Initial check-in of module BMIcalc

---
+++ BMIcalc/BMI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bmiCalculation
+{
+    public class Person
+    {
+        //Private Attributes
+        public string firstName {get; set;}
+        public string lastName { get; set; }
+        public char lastNameInitial { get; set; }
+        public int age { get; set; }
+        public double weight { get; set; }
+        public int feet { get; set; }
+        public int inches { get; set; }
+        public string bmiLevel { get; set; }
+
+        //Constructors with this.attributeName
+        public Person(string firstName, string lastName, double weight, int feet, int
+        inches)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.weight = weight;
+            this.feet = feet;
+            this.inches = inches;
+        }
+
+        public Person(string firstName, char lastNameInitial, double weight, int feet, int
+        inches)
+        {
+            this.firstName = firstName;
+            this.lastNameInitial = lastNameInitial;
+            this.weight = weight;
+            this.feet = feet;
+            this.inches = inches;
+        }
+
+        public Person(string firstName, string lastName, char lastNameInitial, int age, double weight, int feet, int inches)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.lastNameInitial = lastNameInitial;
+            this.age = age;
+            this.weight = weight;
+            this.feet = feet;
+            this.inches = inches;
+        }
+
+        public string calculateBMI()
+        {
+            double heightInInches = feet * 12 ;
+            double BMI = weight * 703 / (heightInInches * heightInInches);
+            string bmiLevel;
+
+            if (BMI < 18.5)
+                bmiLevel = "Underweight";
+            if (BMI > 18.5 || BMI < 24.9)
+                bmiLevel = "Normal";
+            if (BMI > 25 || BMI < 29.9)
+                bmiLevel = "Overweight";
+            else
+                bmiLevel = "Obese";
+            return BMI + " : " + bmiLevel;
+        }
+    }
+}
---
+++ BMIcalc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace bmiCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Tom", 'H', 152.5, 5, 6);
+
+            Console.WriteLine($"{ p.firstName} : {p.calculateBMI()}");
+
+            Person a = new Person("Leslie", "Knope", 'K', 32, 122.9, 4, 1);
+
+            Console.WriteLine($"{ a.firstName} : {a.calculateBMI()}");
+
+            Person s = new Person("Ron", "Swanson", 222.2, 6, 0);
+
+            Console.WriteLine($"{ s.firstName} : {s.calculateBMI()}");
+
+            Person d = new Person("April", "Ludgate", 110.5, 5, 5);
+
+            Console.WriteLine($"{ d.firstName} : {d.calculateBMI()}");
+        }
+    }
+}
